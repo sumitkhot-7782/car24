4a6db4bbc18845c87e890c64cb9e2b1dcfb92510
Sumit Khot <sumitkhot7782@gmail.com>
2026-03-24 18:21:03 +0530

    Update Bookingapi.ts
    add new API

--- cars24/src/lib/Bookingapi.ts
+++ cars24/src/lib/Bookingapi.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://cars-24-clone-net-nextjs.onrender.com/api/Booking";
+const BASE_URL = "https://carsszone.onrender.com/api/Booking";
 
 export const createBooking = async (userid: string, Booking: any) => {
   const response = await fetch(`${BASE_URL}?userId=${userid}`, {
